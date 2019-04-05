3b84277bc931c76933581132ddcdabd1dc1a8e74
Bruno Porreca <brunoporreca@MacBook-Air-de-Bruno.local>
2019-04-04 23:51:30 -0300

    caravan

---
+++ app.js
@@ -0,0 +1,9 @@
+//colocar legenda / tirinhas nas linhas  
+$(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+});
+
+//funcao de time do carrocel
+$('.carousel').carousel({
+  interval: 2000
+});
